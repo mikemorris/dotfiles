537e742f371b30dee10b1d45848fa23b02bead5e
Mike Morris <1149913+mikemorris@users.noreply.github.com>
2023-12-05 22:39:39 -0500

    add lazyvim config

---
+++ config/nvim/lua/plugins/colorscheme.lua
@@ -0,0 +1,19 @@
+return {
+  { "catppuccin/nvim" },
+  { "EdenEast/nightfox.nvim" },
+  { "ellisonleao/gruvbox.nvim" },
+  { "folke/tokyonight.nvim" },
+  { "navarasu/onedark.nvim" },
+  { "rose-pine/neovim" },
+  { "sainnhe/edge" },
+  { "sainnhe/everforest" },
+  { "sainnhe/gruvbox-material" },
+  { "sainnhe/sonokai" },
+
+  {
+    "LazyVim/LazyVim",
+    opts = {
+      colorscheme = "everforest",
+    },
+  },
+}
